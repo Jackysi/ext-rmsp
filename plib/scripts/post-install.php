d791d7f08b935d98c14a44a32c1eb2bbee4d09e1
Vadim Reyder <vreyder@parallels.com>
2012-02-15 15:49:56 +0700

    Create custom buttons on post-install.php

--- plib/scripts/post-install.php
+++ plib/scripts/post-install.php
@@ -1,7 +1,37 @@
 <?php
-echo "post-install.php runned.";
-//$path = realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Model';
-//$files = glob($path . DIRECTORY_SEPARATOR . '*.php');
+require_once 'pm/Loader.php';
+pm_Loader::enableAutoload();
 
-//foreach ($files as $file) {
-//}
+$request = <<<XML
+<ui>
+    <create-custombutton>
+        <owner>
+            <admin/>
+        </owner>
+        <properties>
+            <place>navigation</place>
+            <url>/modules/rmsp/</url>
+            <text>Customer Requests</text>
+        </properties>
+    </create-custombutton>
+</ui>
+XML;
+pm_ApiRpc::getService('1.6.3.0')->call($request);
+
+$request = <<<XML
+<ui>
+    <create-custombutton>
+        <owner>
+            <admin/>
+        </owner>
+        <properties>
+            <public>true</public>
+            <place>domain</place>
+            <url>/modules/rmsp/</url>
+            <text>My Requests</text>
+        </properties>
+    </create-custombutton>
+</ui>
+XML;
+
+pm_ApiRpc::getService('1.6.3.0')->call($request);
