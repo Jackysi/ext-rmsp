724306b6c386cef47379c2c4b6afbccb44e00f8c
Vadim Reyder <vreyder@parallels.com>
2012-03-13 12:59:32 +0700

    Fix module for "Modules SDK Phase II"

--- plib/Model/Abstract.php
+++ plib/Model/Abstract.php
@@ -2,7 +2,6 @@
 /**
  * Abstract Model
  **/
-define('MODEL_PATH', realpath(dirname(__FILE__)));
 
 abstract class modules_rmsp_Model_Abstract
 {
@@ -12,7 +11,11 @@ abstract class modules_rmsp_Model_Abstract
 
     public function __construct()
     {
-        $this->_dbh = new PDO('sqlite:' . MODEL_PATH
-                . DIRECTORY_SEPARATOR . self::DATABASE_NAME);
+        # TODO Remove PRODUCT_ROOT_D and use native consts
+        $this->_dbh = new PDO('sqlite:' . PRODUCT_ROOT_D . DIRECTORY_SEPARATOR
+            . 'var' . DIRECTORY_SEPARATOR
+            . 'modules' . DIRECTORY_SEPARATOR
+            . 'rmsp' . DIRECTORY_SEPARATOR
+            . DIRECTORY_SEPARATOR . self::DATABASE_NAME);
     }
 }
